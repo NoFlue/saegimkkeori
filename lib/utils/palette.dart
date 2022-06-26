7e32891e0562b6593114d2495fd8f4803ae1751f
noflue <kimjh3527@naver.com>
2022-06-26 23:16:43 +0900

    Primary Color 변경

--- lib/utils/palette.dart
+++ lib/utils/palette.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static final Color background = Color(0xfff5f5f5);
-  static final Color primary = Color(0xff999185);
+  static final Color primary = Color(0xff4c4c4c);
 }
