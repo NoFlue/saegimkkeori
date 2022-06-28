efab6d8f0425944e80e5bd13ad11ef977519f19c
noflue <kimjh3527@naver.com>
2022-06-29 05:02:44 +0900

    작성 페이지 작성 중

---
+++ lib/pages/writing_page/memory_writing_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:saegimkkeori/providers/memory_image_picker.dart';
+import 'package:saegimkkeori/utils/palette.dart';
+
+class MemoryWritingPage extends StatefulWidget {
+  const MemoryWritingPage({Key? key}) : super(key: key);
+
+  @override
+  State<MemoryWritingPage> createState() => _MemoryWritingPageState();
+}
+
+class _MemoryWritingPageState extends State<MemoryWritingPage> {
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        StateNotifierProvider<MemoryImagePicker, MemoryImagePickerState>(
+          create: (_) => MemoryImagePicker(),
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: Palette.background,
+        appBar: AppBar(
+          backgroundColor: Palette.background,
+          elevation: .0,
+          iconTheme: IconThemeData(color: Palette.primary),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                '작성',
+                style: TextStyle(
+                  color: Palette.primary,
+                  fontFamily: 'EF_Diary',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '제목',
+                  isDense: true,
+                ),
+                style:
+                    TextStyle(color: Palette.primary, fontFamily: 'EF_Diary'),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 14, right: 14),
+                child: Divider(
+                  thickness: .3,
+                  color: Palette.primary,
+                  height: 0,
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _contentController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: '내용',
+                    isDense: true,
+                  ),
+                  style:
+                      TextStyle(color: Palette.primary, fontFamily: 'EF_Diary'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
